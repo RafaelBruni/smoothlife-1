4649a3a8a4fa11e7f7de81351f8be7c6904e3687
alxpr <alekx@Alek.local>
2016-05-05 00:39:40 +0200

    Massive Update - v1.3
    - secured publications (only orders of current users are accessible and
    only once logged in)
    - new font design
    - new partial layout
    - responsive navigation menu
    - new tab: orders (list of user’s orders)
    - all photos added (public/img)

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ Meteor.publish('ingredientsPublish', function(){
 	return ingredients.find();
 });
 Meteor.publish('smoothiesPublish', function(){
-	return smoothies.find();
+	var currentUserId = this.userId;
+	return smoothies.find({ orderBy: currentUserId });
 });
 Meteor.publish('menuSmoothiesPublish', function(){
 	return menuSmoothies.find();
